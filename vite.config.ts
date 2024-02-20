6a0bb46815bdff6b443e5c6928393bbc78484383
vlGoku <markolucic21@outlook.de>
2024-02-20 08:30:35 +0100

    changed json-server port to 5001, can change back to 5000

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         "/questions": {
-          target: "http://localhost:5000",
+          target: "http://localhost:5001",
           changeOrigin: true,
         },
       },
